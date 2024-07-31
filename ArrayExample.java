41c711b3d3d5a80fea2fbfe2d8cfd8f5af6d78ed
MounikaReddy290 <mounikareddygaddam29@gmail.com>
2024-07-31 22:16:22 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,36 @@
+package apjfsa;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		int[] numbers;
+		numbers = new int[10];
+		//initializing first number
+		  numbers[0] = 10;
+		//initializing second number
+		  numbers[1] = 13;
+		//initializing third number
+		  numbers[2] = 20;	
+		//initializing fourth number
+		  numbers[3] = 65;
+		//initializing fifth number
+		  numbers[4] = 30;
+		//initializing sixth number
+			numbers[5] = 140;
+			//initializing seventh number
+			numbers[6] = 50;
+			//initializing eighth number
+			numbers[7] =60;	
+			//initializing ninth number
+			numbers[8] = 70;
+			//initializing tenth number
+			numbers[9] =80;
+	 for(int i=0;i<=9;i++)
+		 System.out.print(numbers[i] + " ");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
